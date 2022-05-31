80aa3c288d40ef213f70111c5e1ebe97501fb3ca
adekwahyudi <adekwahyudi7@gmail.com>
2022-05-31 20:32:45 +0700

    Tugas Web Programming II

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?= $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Adek</li>
+        <li>Nama Belakang : Wahyudi</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jl kayutinggi Gg garuda 2 </li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olah raga Favorit </h4>
+    <ul type="square">
+        <li>Futsal</li>
+    </ul>
+</section>
